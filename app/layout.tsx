0d2db93cabd5a9ee42ff8ffcc3e542fdf7a222c5
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2025-08-06 09:31:04 +0530

    fix: resolved hydration error and updated font setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,16 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        `}</style>
-      </head>
-      <body>{children}</body>
+      <body className={`${GeistSans.className} ${GeistMono.variable}`}>
+        {children}
+      </body>
     </html>
   )
 }
